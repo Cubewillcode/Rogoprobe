b08cb3f3678b54110b6044aaa5e2e958c3a46100
Jakub <jaco8055@gmail.com>
2018-12-28 04:07:21 +0000

    save load, and extra levels for the player and the dungeon

--- src/render_functions.py
+++ src/render_functions.py
@@ -4,13 +4,14 @@ from enum import Enum
 
 from game_states import GameStates
 
-from menus import inventory_menu
+from menus import character_screen, inventory_menu, level_up_menu
 
 
 class RenderOrder(Enum):
-    CORPSE = 1
-    ITEM = 2
-    ACTOR = 3
+    STAIRS = 1
+    CORPSE = 2
+    ITEM = 3
+    ACTOR = 4
 
 
 def get_names_under_mouse(mouse, entities, fov_map):
@@ -69,7 +70,7 @@ def render_all(drawing_con, panel, entities, player, game_map, fov_map, fov_reco
 
     # Draw all entities in the list
     for entity in entities_in_render_order:
-        draw_entity(drawing_con, entity, fov_map)
+        draw_entity(drawing_con, entity, fov_map, game_map)
 
     libtcod.console_blit(drawing_con, 0, 0, screen_width, screen_height, 0, 0, 0)
 
@@ -85,6 +86,8 @@ def render_all(drawing_con, panel, entities, player, game_map, fov_map, fov_reco
 
     render_bar(panel, 1, 1, bar_width, 'HP', player.fighter.hp, player.fighter.max_hp,
                libtcod.darker_green, libtcod.darker_red)
+    libtcod.console_print_ex(panel, 1, 3, libtcod.BKGND_NONE, libtcod.LEFT,
+                             'Dungeon level: {0}'.format(game_map.dungeon_level))
 
     libtcod.console_set_default_foreground(panel, libtcod.light_gray)
     libtcod.console_print_ex(panel, 1, 0, libtcod.BKGND_NONE, libtcod.LEFT,
@@ -100,14 +103,21 @@ def render_all(drawing_con, panel, entities, player, game_map, fov_map, fov_reco
 
         inventory_menu(drawing_con, inventory_title, player.inventory, 50, screen_width, screen_height)
 
+    elif game_state == GameStates.LEVEL_UP:
+        level_up_menu(drawing_con, 'Level up! Choose a stat to raise:', player, 40, screen_width, screen_height)
+
+    elif game_state == GameStates.CHARACTER_SCREEN:
+        character_screen(player, 30, 10, screen_width, screen_height)
+
 
 def clear_all(drawing_con, entities):
     for entity in entities:
         clear_entity(drawing_con, entity)
 
 
-def draw_entity(drawing_con, entity, fov_map):
-    if libtcod.map_is_in_fov(fov_map, entity.x, entity.y):
+def draw_entity(drawing_con, entity, fov_map, game_map):
+    if libtcod.map_is_in_fov(fov_map, entity.x, entity.y) or (entity.stairs and
+                                                              game_map.tiles[entity.x][entity.y].explored):
         libtcod.console_set_default_foreground(drawing_con, entity.color)
         libtcod.console_put_char(drawing_con, entity.x, entity.y, entity.char, libtcod.BKGND_NONE)
 
